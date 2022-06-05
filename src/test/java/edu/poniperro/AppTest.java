4a40bf5bfe2b3f23bacdb427ff82ca996264d2b6
LESLIE <leslie@MacBook-Air-de-LESLIE.local>
2022-06-05 12:01:02 +0200

    initial push

---
+++ src/test/java/edu/poniperro/AppTest.java
@@ -0,0 +1,11 @@
+package edu.poniperro;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void firstTest() {
+        assertTrue(true);
+    }
+}

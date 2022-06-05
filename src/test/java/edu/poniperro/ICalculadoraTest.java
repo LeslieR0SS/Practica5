1fb48f48bda28a178d2078d4ec9f92076e90e412
LESLIE <leslie@MacBook-Air-de-LESLIE.local>
2022-06-05 12:43:31 +0200

    adding test - 2nd commit

---
+++ src/test/java/edu/poniperro/ICalculadoraTest.java
@@ -0,0 +1,23 @@
+package edu.poniperro;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class ICalculadoraTest {
+
+    @Test
+    public void testSumar(){
+        assertEquals("1+1", 2, ICalculadora.sumar(1,1),0);
+    }
+    @Test
+    public void testRestar(){
+        assertEquals("4-2", 2,ICalculadora.restar(4,2),1 );
+    }
+    @Test
+    public void testMultiplicar(){
+        assertEquals("4x4",16, ICalculadora.multiplicar(4, 4), 0);
+    }
+}
